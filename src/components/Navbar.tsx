8b91c881a6889a78a3d7e0aa738cee3dfe1e8ce0
TriTran <thanhtritran8@gmail.com>
2024-10-08 21:36:14 +0700

    ok

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,34 @@ import Image from "next/image";
 import Logo from "../../public/logo.png";
 import { HomeClientComponent } from "./homeClientComponent";
 import Link from "next/link";
+import Shopee from "../../public/shopee.png"
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const Navbar = () => {
   return (
     <nav className="w-full h-14 border-b px-4 md:px-8 flex flex-row justify-between items-center">
-      <Link href={`/`}>
-      <Image src={Logo} alt={ "Logo" } className="h-14 w-28 object-contain" />
-      </Link>
+      <div className="flex gap-8">
+        <Link href={`/`}>
+          <Image src={Logo} alt={"Logo"} className="h-14 w-28 object-contain" />
+        </Link>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Image src={Shopee} alt="shopee" className="w-8 h-8" />
+            </TooltipTrigger>
+            <TooltipContent>
+            <Image src={Shopee} alt="shopee" className="w-32 h-32" />
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
       <HomeClientComponent />
     </nav>
   );
