cebd388877e9bea16df56a24f9647163540535b7
TriTran <thanhtritran8@gmail.com>
2024-10-08 21:09:54 +0700

    update profile

--- src/app/update-profile/page.tsx
+++ src/app/update-profile/page.tsx
@@ -1,6 +1,35 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 const page = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClickSave = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      router.push("/");
+    }, 1000);
+  };
+
   return (
-    <div></div>
+    <div className="flex flex-col justify-center items-center py-8 gap-4">
+      <h1 className="font-bold text-3xl">Thông tin</h1>
+      <div className="flex flex-col gap-2 w-[500px] ">
+        <label>Tên</label>
+        <input type="text" className="border rounded-lg" />
+        <label>Số điện thoại</label>
+        <input type="text" className="border rounded-lg" />
+        <label>Địa chỉ</label>
+        <input type="text" className="border rounded-lg" />
+      </div>
+      <Button onClick={handleClickSave}>
+        {isLoading ? <Loader2 className="animate-spin" /> : <>Lưu</>}
+      </Button>
+    </div>
   );
 };
 
